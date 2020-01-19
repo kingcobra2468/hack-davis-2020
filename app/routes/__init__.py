cdaea8830fd139f26dc7b6689733d803aae8b947
kingcobra2468 <eriktyryshkin@gmail.com>
2020-01-19 01:04:04 -0800

    v1.1

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,2 @@
-from .image_ops import image_ops_blueprint
-from .image_feed import image_feed_blueprint
-from .discovery import discovery_blueprint
 from .homepage import home_blueprint
 from .infographic import graphic_blueprint
